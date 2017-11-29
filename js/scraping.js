d9be6ded4d3c9e2383a23bd95f0e7d0d2cff8d4c
joedoe <d_joe@gmx.net>
2017-11-29 22:03:40 +0100

    clean up

--- js/scraping.js
+++ js/scraping.js
@@ -1,31 +1,31 @@
 var subreddits = ["java", "learnjava", "programming", "learnprogramming", "python", "learnpython"];
 var input;
 
-$(function(){	
-	$("#r_form").change(function() {
-		if(subreddits.indexOf($(":checked").val()) != -1){
+$(function(){
+    $("#r_form").change(function() {
+        if(subreddits.indexOf($(":checked").val()) != -1){
             input = $(":checked").val();
         }
-		$.ajax({
-			url: "php/scraping.php",
-			data: {
-					subreddit: input
-			},
-			beforeSend: function() {
-				$("#output").addClass("loading-icon");
-				$("#output").html("<i class='fa fa-spinner fa-pulse'></i>");
-			},
-			error: function() {
-				$("#output").html("Failed...").show("slow");
-			},
-			success: function (data) {
-				$("#output").removeClass("loading-icon");
-				$("#output").html(data).show();
-			}
-		});
-	});
+        $.ajax({
+            url: "php/scraping.php",
+            data: {
+                    subreddit: input
+            },
+            beforeSend: function() {
+                $("#output").addClass("loading-icon");
+                $("#output").html("<i class='fa fa-spinner fa-pulse'></i>");
+            },
+            error: function() {
+                $("#output").html("Failed...").show("slow");
+            },
+            success: function (data) {
+                $("#output").removeClass("loading-icon");
+                $("#output").html(data).show();
+            }
+        });
+    });
 });
 
 function startScraper(){
-	$("#r_form").trigger("change");
+    $("#r_form").trigger("change");
 }
